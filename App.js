6322c0316900a10cd0ee3bffe196ed864657e25b
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-08-29 09:36:54 +0530

    bb

--- App.js
+++ App.js
@@ -2,32 +2,37 @@ import cors from "cors";
 import "dotenv/config";
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
-
-
+ 
 //express
-const app = express()
-
-
+const app = express(); 
+ 
 //Middlewares
-app.use(cors())
-app.use(urlencoded({extended:true}))
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.static("public"))
+app.use(
+  cors({ 
+    origin: [ 
+      "http://localhost:3000/",
+      "https://bloghive-lovat.vercel.app/",
+      "https://bloghive-git-main-himanshipatel0409gmailcoms-projects.vercel.app/",
+    ],
+  })
+);
 
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
 
 //routes
-import userRoutes from "./routes/user.routes.js"
-import blogRoutes from "./routes/blog.routes.js"
-import likeRoutes from "./routes/like.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import blogRoutes from "./routes/blog.routes.js";
+import likeRoutes from "./routes/like.routes.js";
 
-app.use("/api/v1/users",userRoutes)
+app.use("/api/v1/users", userRoutes);
 
-app.use("/api/v1/blogs",blogRoutes)
+app.use("/api/v1/blogs", blogRoutes);
 
-app.use("/api/v1/likes",likeRoutes)
-
+app.use("/api/v1/likes", likeRoutes);
 
 // sendEmail()
 
-export {app}
+export { app };
